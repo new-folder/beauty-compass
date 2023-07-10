5b4360de9477dbd92efcc9b8e14b79c6ca600874
Maria <newoptim@gmail.com>
2023-07-10 03:26:15 +0300

    edit brand

--- js/script.js
+++ js/script.js
@@ -197,59 +197,74 @@ const hideTagList = (tagList, indexStartHide) => {
 
 hideTagListInit(2)
 
-// Authorization
+function userInterface() {
+    let userId
 
-function saveUserToken() {
-    const authForm = document.querySelector('.auth__form')
-    console.log(authForm)
-    authForm.onsubmit = async function(e) {
-        // const formData = new FormData(e.target)
-        const emailValue = document.querySelector('#headerAuthEmail')
-        const passValue = document.querySelector('#authInputPassword')
-        e.preventDefault()
-        const authBody = { 
-            // username: emailValue,
-            // password: passValue,
-            username: 'kminchelle',
-            password: '0lelplR',
-        }
-        const resp = await fetch('https://dummyjson.com/auth/login',{
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(authBody)
-        })
-        const result = await resp.json()
-        localStorage.setItem('userToken', result['token'])
+    // Authorization
+    function saveUserLocalStorage() {
+        const authForm = document.querySelector('.auth__form')
+        authForm.onsubmit = async function(e) {
+            // const formData = new FormData(e.target)
+            const emailValue = document.querySelector('#headerAuthEmail')
+            const passValue = document.querySelector('#authInputPassword')
+            e.preventDefault()
+            const authBody = { 
+                // email: emailValue,
+                // password: passValue,
+                username: 'kminchelle',
+                password: '0lelplR',
+            }
+            const resp = await fetch('https://dummyjson.com/auth/login',{
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify(authBody)
+            })
+            const result = await resp.json()
+            localStorage.setItem('userToken', result['token'])
+            localStorage.setItem('userId', result['id'])
+            console.log(userId)
+        }    
     }
+    saveUserLocalStorage()  
+
+    // Add/remove class 'active' for favorite-brand
+    async function toggleFavoriteBrand() {
+        const favoriteBrand = document.querySelector('.favorite--brand')
+        const userToken = localStorage.getItem('userToken')
+        if (userToken) {
+            const resp = await fetch('https://64a990b78b9afaf4844ad897.mockapi.io/favoritebrand')
+            const result = await resp.json()
+            userObject = result.find(elem => elem.id === userId)
+            if (userObject['favorite']) {
+                favoriteBrand.classList.add('active')
+            }
+        } else {
+            console.log('Unauth')
+        }
+    }    
+    toggleFavoriteBrand()    
+
 }
+userInterface()
 
-saveUserToken()
 
-
-const userToken = localStorage.getItem('userToken')
-if (userToken) {
-    console.log('User auth')
-} else {
-    console.log('Unauth')
-}
-    
-
-// Add/remove class 'active' for favorite-brand
-function toggleFavoriteBrand() {
-    const favoriteList = document.querySelectorAll('.favorite--brand')
-    favoriteList.forEach(item => {
-        item.addEventListener('click', elem => {
-            if(!elem.target.classList.contains('active')) {
-                elem.target.classList.add('active')
-            // сюда запрос для добавления в избранное
-            } else
-            elem.target.classList.remove('active')
-            // сюда запрос для удаления из избранного
-        }) 
-    })
-}
-
-toggleFavoriteBrand()
+// async function toggleFavoriteBrand() {
+//     const favoriteBrand = document.querySelector('.favorite--brand')
+//     const userToken = localStorage.getItem('userToken')
+//     let userId = '15'
+//     if (userToken) {
+//         const resp = await fetch('https://64a990b78b9afaf4844ad897.mockapi.io/favoritebrand')
+//         const result = await resp.json()
+//         userObject = result.find(elem => elem.id === userId)
+//         console.log(userObject['favorite'])
+//         if (userObject['favorite']) {
+//             favoriteBrand.classList.add('active')
+//         }
+//     } else {
+//         console.log('Unauth')
+//     }
+// }    
+// toggleFavoriteBrand()   
 
 
 // Add/remove class 'active' for favorite-product
