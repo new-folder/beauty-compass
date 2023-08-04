fbd71be90b6992e2385a54c88dfe2aa7bde32044
lelikone777 <2257855@gmail.com>
2023-08-04 13:55:36 +0400

    add info page

--- js/manufacturer.js
+++ js/manufacturer.js
@@ -37,70 +37,132 @@ tippy('.right__question', {
   allowHTML: true,
 });
 
-// input file
-function triggerFileInputProfile() {
-  const profileFileInput = document.getElementById('profile__file-input');
-  profileFileInput.click();
-}
-function triggerFileInputShop() {
-  const shopFileInput = document.getElementById('shop__file-input');
-  shopFileInput.click();
-}
-
-function handleFileSelected(event) {
-  const file = event.target.files[0];
-  if (file) {
-    // Обработка загруженного файла
-    console.log('Загруженный файл:', file.name);
-  }
-}
 
 // creating brands
-const addInfoSchemes = document.querySelector('.add-info__schemes');
-const addInfoSchemeLeft = document.querySelector('.add-info__schemes .left .left-inner');
-const addInfoSchemeRight = document.querySelector('.add-info__schemes .right .right-inner');
 
-const manufacturersList = []
-const brands = [];
+// const addInfoSchemeLeft = document.querySelector('.add-info__schemes .left .left-inner');
+// const addInfoSchemeRight = document.querySelector('.add-info__schemes .right .right-inner');
+//
+// const addItem = () => {
+//   event.preventDefault();
+//   const leftItem = document.createElement('div');
+//   leftItem.classList.add('left-inner__item');
+//   const brandItem = document.createElement('div');
+//   brandItem.classList.add('left-inner__brand', 'title--h5', 'hover--red',
+//     'd-flex', 'align-items-end', 'ps-2');
+//   brandItem.id = 'left-inner__brand-' + addInfoSchemeLeft.childElementCount + 1
+//
+//   const rightItem = document.createElement('div');
+//   rightItem.classList.add('right-inner__item');
+//   // rightItem.classList.add('my-3')
+//   rightItem.innerHTML = `
+//     <a
+//       href=""
+//       class="right__title title--h5 hover--red text-decoration-none position-relative">
+//       <span>Бренд ${addInfoSchemeRight.childElementCount + 1}</span>
+//       <img class="question right__question d-none d-md-inline mx-2" src="../img/plus-rect.svg" alt="question logo">
+//     </a>
+//     `
+//   const imgSrc = addInfoSchemeLeft.childElementCount === 0
+//     ? '../img/1-level.svg'
+//     : '../img/1-level2.svg';
+//
+//     brandItem.innerHTML = `
+//
+//       <img src="${imgSrc}" alt="img">
+//       <span
+//         class="ps-2"
+//         role='button'
+//         >
+//         Бренд ${addInfoSchemeLeft.childElementCount + 1}
+//       </span>
+//     `
+//   leftItem.appendChild(brandItem)
+//
+//
+//   brandItem.addEventListener('click', () => {
+//       event.preventDefault();
+//       addSeries(leftItem);
+//     });
+//
+//   addInfoContent.style.maxHeight = (addInfoContent.scrollHeight + 50) + 'px';
+//   addInfoSchemeLeft.classList.add('pt-4')
+//   addInfoSchemeLeft.appendChild(leftItem);
+//   addInfoSchemeRight.appendChild(rightItem);
+// }
+//
+// const addSeries = (parentItem) => {
+//   const inner2lvlItem = document.createElement('div');
+//   inner2lvlItem.classList.add('left-inner__2lvl');
+//   inner2lvlItem.innerHTML = `
+//   <div
+//     class="title--h5 hover--red d-flex align-items-end"
+//     role='button'
+//     style="padding-left: 2rem;"
+//     onclick={event.preventDefault()}
+//     >
+//     <img src="../img/2-level.svg" alt="img">
+//     <span class="ps-2">Серия ${parentItem.childElementCount}</span>
+//   </div>
+//   `
+//   addInfoContent.style.maxHeight = (addInfoContent.scrollHeight + 50) + 'px';
+//   addInfoSchemeLeft.classList.add('pt-4')
+//   parentItem.appendChild(inner2lvlItem);
+// }
 
-
-const addSeries = () => {
-  event.preventDefault();
-  const item = document.createElement('div');
-  item.classList.add('left-inner__2lvl');
-  item.innerHTML = `
-  <div class="title--h5 hover--red d-flex align-items-end" role='button' style="padding-left: 2rem;">
-    <img src="../img/2-level.svg" alt="img">
-    <span class="ps-2">Серия 1</span>
-  </div>
-  `
-  addInfoSchemeLeft.appendChild(item);
-  addInfoContent.style.maxHeight = (addInfoContent.scrollHeight + 50) + 'px';
-  addInfoSchemeLeft.classList.add('pt-4')
-}
+const addInfoInner = document.querySelector('.add-info__inner');
 
 const addItem = () => {
   event.preventDefault();
-  const item = document.createElement('div');
-  item.classList.add('left-inner__item');
 
-  if (addInfoSchemeLeft.childElementCount === 0) {
-    item.innerHTML = `
-    <div class="title--h5 hover--red d-flex align-items-end ps-2">
-      <img src="../img/1-level.svg" alt="img">
-      <span class="ps-2" onclick={addSeries()} role='button'>Бренд1</span>
-    </div>`
-  }
-  if (addInfoSchemeLeft.childElementCount > 0) {
-    item.innerHTML = `
-    <div class="title--h5 hover--red d-flex align-items-end ps-2">
-      <img src="../img/1-level2.svg" alt="img">
-      <span class="ps-2" onclick={addSeries()} role='button'>Бренд2</span>
-    </div>`
-  }
+  const addInfoRow = document.createElement('div');
+  addInfoRow.classList.add('add-info__row', 'd-flex');
 
-  addInfoContent.style.maxHeight = (addInfoContent.scrollHeight + 50) + 'px';
-  addInfoSchemeLeft.classList.add('pt-4')
-  addInfoSchemeLeft.appendChild(item);
+  const addInfoLeft = document.createElement('div');
+  addInfoLeft.classList.add('add-info__left', 'col-6 border--right', 'pt-4');
+
+  const addInfoRight = document.createElement('div');
+  addInfoRight.classList.add('add-info__right', 'col-6', 'ps-4', 'pt-4');
+
+  const leftItem = document.createElement('div');
+  leftItem.classList.add('left-inner__item', 'title--h5', 'hover--red', 'd-flex', 'align-items-end', 'ps-2');
+
+  const rightItem = document.createElement('div');
+  rightItem.classList.add('right-inner__item', 'title--h5', 'hover--red', 'd-flex', 'align-items-end', 'ps-2');
+
+
+
+
+
+
 }
 
+
+
+// modal price
+const modalPrices = document.getElementById('modal--prices');
+const modalPricesConfirm = document.getElementById('modal--confirm');
+const modalPricesCells = modalPrices.querySelectorAll('.table tr td');
+
+modalPricesCells.forEach((cell) => {
+
+  cell.setAttribute('data-bs-dismiss', 'modal');
+  cell.setAttribute('data-bs-toggle', 'modal');
+  cell.setAttribute('href', '#modal--confirm');
+
+  cell.addEventListener('click', () => {
+
+    console.log(cell)
+
+    const cellElement = cell.querySelector('span');
+    if (cellElement) {
+      const cellPrice = cellElement.textContent.trim();
+      const cellNumber = parseInt(cellPrice);
+      if (!isNaN(cellNumber)) {
+        console.log('Число внутри ячейки:', cellNumber);
+      } else {
+        console.log('Содержимое внутри ячейки не является числом:', cellPrice);
+      }
+    }
+  })
+})
