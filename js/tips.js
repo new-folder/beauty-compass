1554145e97bc245d322e0c3ca76ab5ee29f2ed29
lelikone777 <2257855@gmail.com>
2023-08-20 02:19:01 +0400

    7

---
+++ js/tips.js
@@ -0,0 +1,17 @@
+tippy(".left__question", {
+  content: '<div class="cloud">Пример текста</div>',
+  theme: "light",
+  placement: "top-start",
+  arrow: false,
+  allowHTML: true,
+});
+
+tippy(".right__question", {
+  content:
+    '<div class="cloud">Вы можете задать порядок отображения средств перемещая их иконки</div>',
+  theme: "light",
+  placement: "top-start",
+  arrow: false,
+  allowHTML: true,
+});
+
