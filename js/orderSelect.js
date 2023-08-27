17918953891f86bdb90f6a2837503a4a626dbde7
lavash2008 <p-bayandin03@mail.ru>
2023-08-27 12:47:01 +0500

    modified:   css/modules/orderSelect.css 	modified:   css/modules/orderSelect.sass 	modified:   css/modules/test.css.map 	modified:   css/modules/test.sass 	new file:   css/select2.min.css 	modified:   css/style.css 	modified:   css/style.css.map 	modified:   css/style.sass 	modified:   index.html 	new file:   js/jq.js 	new file:   js/orderSelect.js 	new file:   js/select2.js 	modified:   orderSelect.html

---
+++ js/orderSelect.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#activeIgnore').select2({
+        closeOnSelect: false,
+        placeholder:"Выберите из списка" 
+    }
+    );
+    $('#skinProp').select2({
+        closeOnSelect: false,
+        placeholder:"Выберите из списка" 
+    }
+    );
+    $('#price').select2({
+        closeOnSelect: false,
+        placeholder:"Выберите из списка" 
+    }
+    );
+})
