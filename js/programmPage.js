408742794ae28db52acf515461d1cd7b084e1cd5
lavash2008 <p-bayandin03@mail.ru>
2023-08-30 22:34:09 +0500

    new file:   css/modules/buttonStyle.sass 	new file:   css/modules/index.css 	new file:   css/modules/index.css.map 	modified:   css/modules/index.sass 	new file:   css/modules/inputText.css 	new file:   css/modules/inputText.sass 	new file:   css/modules/select2.sass 	new file:   css/modules/test.sass 	modified:   css/style.css 	modified:   css/style.css.map 	modified:   css/style.sass 	modified:   index.html 	modified:   js/orderSelect.js 	deleted:    js/programmPage.js 	deleted:    js/rezultTest.js 	deleted:    js/test.js 	modified:   prepros.config

--- js/programmPage.js
+++
@@ -1,9 +0,0 @@
-// redirection and ajax 
-
-document.getElementById('butSave').addEventListener('click',function(event){
-    event.preventDefault();
-    //place ajax
-    
-    location.replace("saveProgramm.html");
-    
-});
--- js/rezultTest.js
+++
@@ -1,9 +0,0 @@
-// redirection and ajax 
-
-document.getElementById('butChoice').addEventListener('click',function(event){
-    event.preventDefault();
-    //place ajax
-    
-    location.replace("orderSelect.html");
-    
-});
