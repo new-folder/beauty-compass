614fe927e77135c7bf4f5863ecd3355cc1042cfa
lavash2008 <p-bayandin03@mail.ru>
2023-09-02 13:06:56 +0500

    modified:   chatTech.html 	modified:   css/modules/index.css 	modified:   css/modules/index.css.map 	modified:   css/modules/index.sass 	modified:   css/style.css 	modified:   css/style.css.map 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -10,5 +10,11 @@ $(document).ready(function() {
         location.replace('chatTech.html');
     })
 
-    
+    $('#saveProfile').on('click', function(){
+        const data=new FormData($('.persData')[0]);
+        file=data.get('form__pic')
+        if (file) {
+            console.log("Загруженный файл:", file.name);
+        }
+    })
 })
