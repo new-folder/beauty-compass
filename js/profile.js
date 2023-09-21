ae00065a69bba9e8c664b50bc7c7922ea5e82132
lavash2008 <p-bayandin03@mail.ru>
2023-09-21 22:51:52 +0500

    modified:   css/modules/_basic.sass 	new file:   css/modules/details.css 	new file:   css/modules/details.sass 	modified:   css/modules/manufacturer.sass 	modified:   css/modules/pagin.sass 	modified:   css/style.sass 	modified:   js/profile.js 	modified:   lk-users/chat-persons.html 	modified:   lk-users/chat-tech.html 	modified:   lk-users/comments.html 	modified:   manufacturer-lk/profile.html 	modified:   test-pages/saveProgramm.html

--- js/profile.js
+++ js/profile.js
@@ -7,7 +7,7 @@ let isOpen = false;
 
 addInfoTrigger.addEventListener("click", function () {
   if (!isOpen) {
-    addInfoContent.style.maxHeight = addInfoContent.scrollHeight + 20 + "px";
+    addInfoContent.style.maxHeight = "none";
     isOpen = true;
     addInfoArrow.style.transform = "rotate(180deg)";
     addInfoContent.style.margin = "1.5rem 0 1rem 0";
@@ -21,22 +21,22 @@ addInfoTrigger.addEventListener("click", function () {
 });
 
 // tooltip
-tippy(".left__question", {
-  content: '<div class="cloud">Пример текста</div>',
-  theme: "light",
-  placement: "top-start",
-  arrow: false,
-  allowHTML: true,
-});
+// tippy(".left__question", {
+//   content: '<div class="cloud">Пример текста</div>',
+//   theme: "light",
+//   placement: "top-start",
+//   arrow: false,
+//   allowHTML: true,
+// });
 
-tippy(".right__question", {
-  content:
-    '<div class="cloud">Вы можете задать порядок отображения средств перемещая их иконки</div>',
-  theme: "light",
-  placement: "top-start",
-  arrow: false,
-  allowHTML: true,
-});
+// tippy(".right__question", {
+//   content:
+//     '<div class="cloud">Вы можете задать порядок отображения средств перемещая их иконки</div>',
+//   theme: "light",
+//   placement: "top-start",
+//   arrow: false,
+//   allowHTML: true,
+// });
 
 // creating brands
 
@@ -169,4 +169,84 @@ modalPricesCells.forEach((cell) => {
       }
     }
   });
+});
+
+class Accordion {
+  constructor(el) {
+    this.el = el;
+    this.summary = el.querySelector('summary');
+    this.content = el.querySelector('.content');
+
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.summary.addEventListener('click', (e) => this.onClick(e));
+  }
+
+  onClick(e) {
+    e.preventDefault();
+    this.el.style.overflow = 'hidden';
+    if (this.isClosing || !this.el.open) {
+      this.open();
+    } else if (this.isExpanding || this.el.open) {
+      this.shrink();
+    }
+  }
+
+  shrink() {
+    this.isClosing = true;
+    
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight}px`;
+    
+    if (this.animation) {
+      this.animation.cancel();
+    }
+    
+    this.animation = this.el.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 400,
+      easing: 'ease-out'
+    });
+    this.animation.onfinish = () => this.onAnimationFinish(false);
+    this.animation.oncancel = () => this.isClosing = false;
+  }
+
+  open() {
+    this.el.style.height = `${this.el.offsetHeight}px`;
+    this.el.open = true;
+    window.requestAnimationFrame(() => this.expand());
+  }
+
+  expand() {
+    this.isExpanding = true;
+    const startHeight = `${this.el.offsetHeight}px`;
+    const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`;
+    
+    if (this.animation) {
+      this.animation.cancel();
+    }
+    
+    this.animation = this.el.animate({
+      height: [startHeight, endHeight]
+    }, {
+      duration: 400,
+      easing: 'ease-out'
+    });
+    this.animation.onfinish = () => this.onAnimationFinish(true);
+    this.animation.oncancel = () => this.isExpanding = false;
+  }
+
+  onAnimationFinish(open) {
+    this.el.open = open;
+    this.animation = null;
+    this.isClosing = false;
+    this.isExpanding = false;
+    this.el.style.height = this.el.style.overflow = '';
+  }
+}
+
+document.querySelectorAll('.add-info__schemes details').forEach((el) => {
+  new Accordion(el);
 });
