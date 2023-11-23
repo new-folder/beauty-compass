264ab300becb0c49c07330f6a87c6ed4568a0557
Maria <newoptim@gmail.com>
2023-11-23 23:38:23 +0300

    add styles to buttons

--- js/multiple-image-upload.js
+++ js/multiple-image-upload.js
@@ -1,6 +1,6 @@
 const fileInput = document.getElementById('img-multiple');
-const imageContainer = document.getElementById('imageContainer');
-const imageContainerHeader = document.querySelector('.image-container-header');
+const imageContainer = document.querySelector('.added-imgs__container');
+const imageContainerHeader = document.querySelector('.added-imgs__header');
 
 let containerCounter = 0;
 
@@ -21,35 +21,50 @@ fileInput.addEventListener('change', (e) => {
           imageContainerHeader.style.display = 'flex';
         }
 
-        const containerNumber = document.createElement('span');
-        containerNumber.textContent = `${containerCounter + 1}: `;
+        const containerNumber = document.createElement('div');
+        containerNumber.classList.add('col-3');
+        containerNumber.textContent = `${containerCounter + 1} `;
         containerCounter++;
+
+        const checkboxContainer = document.createElement('div');
+        checkboxContainer.classList.add('col-3');
 
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
+        checkbox.classList.add('added-imgs__check');
         checkbox.id = `copyright-${i}`;
 
-        const label = document.createElement('label');
-        label.textContent = 'Добавить копирайт';
-        label.htmlFor = `copyright-${i}`;
+        const question = document.createElement('span');
+        question.classList.add('added-imgs__question');
+        question.textContent = 'Поставить копирайт?';
+        question.htmlFor = `copyright-${i}`;
+
+        checkboxContainer.appendChild(checkbox);
 
         const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Удалить';
-        deleteButton.classList.add('delete-button');
+        deleteButton.classList.add('added-imgs__delete');
+
+        const col3Wrapper = document.createElement('div');
+        col3Wrapper.classList.add('col-3');
+        col3Wrapper.appendChild(deleteButton);
 
         deleteButton.addEventListener('click', () => {
-          const imageContainerItem = deleteButton.parentNode;
+          const imageContainerItem = deleteButton.parentNode.parentNode;
           imageContainer.removeChild(imageContainerItem);
           updateContainerNumbers();
         });
 
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add('col-3');
+        imageWrapper.appendChild(image);
+
         const imageContainerItem = document.createElement('div');
-        imageContainerItem.classList.add('image-container');
+        imageContainerItem.classList.add('added-imgs__item');
         imageContainerItem.appendChild(containerNumber);
-        imageContainerItem.appendChild(image);
-        imageContainerItem.appendChild(checkbox);
-        imageContainerItem.appendChild(label);
-        imageContainerItem.appendChild(deleteButton);
+        imageContainerItem.appendChild(imageWrapper);
+        imageContainerItem.appendChild(question);
+        imageContainerItem.appendChild(checkboxContainer);
+        imageContainerItem.appendChild(col3Wrapper);
 
         imageContainer.appendChild(imageContainerItem);
 
@@ -64,16 +79,16 @@ fileInput.addEventListener('change', (e) => {
 // Используем библиотеку SortableJS
 const sortable = new Sortable(imageContainer, {
   animation: 150,
-  handle: '.image-container',
-  draggable: '.image-container',
+  handle: '.added-imgs__item',
+  draggable: '.added-imgs__item',
   onEnd: updateContainerNumbers,
 });
 
 // Функция для обновления порядковых номеров
 function updateContainerNumbers() {
-  const imageContainers = document.querySelectorAll('.image-container');
+  const imageContainers = document.querySelectorAll('.added-imgs__item');
   imageContainers.forEach((container, index) => {
-    const containerNumber = container.querySelector('span:first-child');
-    containerNumber.textContent = `${index + 1}: `;
-  })
+    const containerNumber = container.querySelector('div:first-child');
+    containerNumber.textContent = `${index + 1}`;
+  });
 }
